c787870060093e5ea9c2da6d6621cbc5a36fd1e2
jasbleidy <andreapilonieta35@gmail.com>
2021-09-18 08:53:17 -0500

    jasbleidy

---
+++ trabajosm.py
@@ -0,0 +1,31 @@
+"""
+este codigo permite tener una conversacion
+18/09/2021
+"""
+
+def pedirdatosnombre ():
+    print ("bienvenid@")
+    print ("¿como te llamas?")
+    nombre = input ()
+    return nombre
+
+nombre=pedirdatosnombre()
+
+def emociones ():
+    print ("¿como te encuentras?")
+    emocion = input ()
+    return nombre
+
+emocion=emociones()
+
+def pedirdatosedad ():
+    print ("¿cuantos años tienes?")
+    edad = input ()
+    return edad
+
+edad=pedirdatosedad()
+
+
+
+
+    
